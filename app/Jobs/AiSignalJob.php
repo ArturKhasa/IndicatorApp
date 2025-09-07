deb6b0fb86105eebaf714507aff4c6eb6ba944d3
arturhasahanov <arturbk2@yandex.ru>
2025-09-07 13:25:08 +0300

    fx

--- app/Jobs/AiSignalJob.php
+++ app/Jobs/AiSignalJob.php
@@ -74,7 +74,7 @@ class AiSignalJob implements ShouldQueue
             ]
         );
 
-        if($signal->action == 'buy' && (float)$data['confidence'] >= 0.78) {
+        if($signal->action == 'buy' && (float)$data['confidence'] >= 0.76) {
             ExecuteAutoBuyJob::dispatch($signal->symbol, '5.0', $signal->reasoning);
         }
     }
